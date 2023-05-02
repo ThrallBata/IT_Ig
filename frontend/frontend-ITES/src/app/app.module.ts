d66373cb3c1cdd097bf145574ef811ac9aa51260
Igor Muromov <igran2001@gmail.com>
2023-05-02 16:22:29 +0700

    Add components ang pages

--- frontend/frontend-ITES/src/app/app.module.ts
+++ frontend/frontend-ITES/src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ProjectItemComponent } from './components/project-item/project-item.component';
+import { OrderItemComponent } from './components/order-item/order-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    LoginPageComponent,
+    ProjectsPageComponent,
+    ProfilePageComponent,
+    NavigationComponent,
+    ChatComponent,
+    LoginFormComponent,
+    ProjectItemComponent,
+    OrderItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

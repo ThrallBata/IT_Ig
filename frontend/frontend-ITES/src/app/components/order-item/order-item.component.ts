9e3ec52d3451a2ce593cfb1a7b9031f272983b10
Igor Muromov <igran2001@gmail.com>
2023-05-05 02:23:47 +0700

    Created site header

--- frontend/frontend-ITES/src/app/components/order-item/order-item.component.ts
+++ frontend/frontend-ITES/src/app/components/order-item/order-item.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-order-item',
   templateUrl: './order-item.component.html',
-  styleUrls: ['./order-item.component.css']
+  styleUrls: ['./order-item.component.scss']
 })
 export class OrderItemComponent {
 
--- frontend/frontend-ITES/src/app/pages/projects-page/projects-page.component.ts
+++ frontend/frontend-ITES/src/app/pages/projects-page/projects-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
-  styleUrls: ['./projects-page.component.css']
+  styleUrls: ['./projects-page.component.scss']
 })
 export class ProjectsPageComponent {
 

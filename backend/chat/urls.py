475232c8dc9df99ba5407fcab54446f1d6eb4512
thrallbata <vicortobdel@gmail.com>
2023-05-05 13:15:10 +0700

    add messagwlist filter user

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -3,7 +3,7 @@ from .views import *
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    path("", index, name="chat"),
     path("<str:room_name>/", room, name="room"),
-    path('messages/', MessageList.as_view()),
 ]
 

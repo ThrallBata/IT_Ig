0dca6d0092465533375ecc09871b4dc0ef3c59a0
ThrallBata <vicortobdel@gmail.com>
2023-06-26 21:28:03 +0700

    update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_AUTH")
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ["https://ites.onrender.com/"]
+CSRF_TRUSTED_ORIGINS = ["https://ites.onrender.com"]
 
 # Application definition
 

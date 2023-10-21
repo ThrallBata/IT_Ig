76ed2b3abc0a8e4c6cc83d572ba028f4f1f25bed
ThrallBata <vicortobdel@gmail.com>
2023-10-21 14:27:17 +0700

    update User model and add response

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -24,12 +24,10 @@ class AuthenticationAPIView(APIView):
     def post(self, request):   # получения номера телефона, создание и отправка кода
         serializer = AuthenticationSerializer(data=request.data)
 
-#TODO поставить валидацию телефона в модели
-
         if serializer.is_valid():
             phone = serializer.data.get('phone')
 
-            if User.objects.filter(phone_number=phone).exists() == True:
+            if User.objects.filter(phone=phone).exists() == True:
                 if redis_auth_code.exists(phone) == 0:
                     authcode = create_auth_code(phone)
                     send_authcode.delay(phone, authcode)
@@ -56,14 +54,14 @@ class AuthenticationCodeAPIView(APIView):
                     return Response(TokenSerializer(
                         {'phone': phone, 'token': token_dict['jwt'],
                          'token_refresh': token_dict['refresh']}, ).data)
-
-        return Response({'error': 'неверные данные',}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'responce': 'Incorrect data'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AuthenticationRefreshTokenAPIView(APIView):
     def post(self, request):
         serializer = AuthenticateRefreshTokenSerializer(data=request.data)
-        print(request.data)
+
         if serializer.is_valid():
 
             token_refresh_elem = serializer.data.get('token_refresh')
@@ -79,5 +77,5 @@ class AuthenticationRefreshTokenAPIView(APIView):
                 if token_refresh_elem == token_refresh_redis:
                     token = get_tokens(phone)
                     return Response({'token': token['jwt'], 'token_refresh': token['refresh']}, )
-
-        return Response({'error': 'Неверные данные', }, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'responce': 'Incorrect data'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
